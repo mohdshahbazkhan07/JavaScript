16d0241e50eb7598e35c9808a99fe50603049901
Mohd Shahbaz Khan <mohdshahbazkhan0707@gmail.com>
2026-08-13 21:03:12 +0000

    datatypes

---
+++ 01-basics/lec3_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+alert(3+3) // we are using nodejs not browser
+
+console.log("Shazy");
+console.log(3+3)
+
+let name = "shazy"
+let age = 18
+let isLoggedIn = false
+/*
+Data types
+
+number=>2 to power 53
+bigint
+string = ""
+boolean
+null = standalone value
+undefined = value not defined yet
+symbol = unique
+
+*/
+
+//typeof
+
+console.log(typeof undefined)
+console.log(typeof null) //object
+console.log(typeof age)
